feefdf4e4aa028a41f5e850f67a354ea6c11f405
rafifathurr <rafitami1008>
2023-07-21 22:31:47 +0700

    Update diff id

--- public/js/app/table.js
+++ public/js/app/table.js
@@ -3,6 +3,10 @@ $('#add-row').DataTable({
 	"pageLength": 10,
 });
 
+$('#add-row-2').DataTable({
+	"pageLength": 10,
+});
+
 var action = '<td> <div class="form-button-action"> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-simple-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-simple-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
 
 $('#addRowButton').click(function() {
